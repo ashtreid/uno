184deea1c7a310d4823bad1be651561aedc31644
Park <parkerlamarbrook@gmail.com>
2023-10-03 19:58:26 -0600

    added UPDATE_GAMES and UPDATE_WINS mutations in client-side mutations.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,24 +1,14 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USERS = gql`
-  query getUsers {
-    users {
-      _id
-      username
-      email
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-    }
-  }
-`;
+// export const QUERY_USER = gql`
+//   query user($username: String!) {
+//     user(username: $username) {
+//       _id
+//       username
+//       email
+//     }
+//   }
+// `;
 
 export const QUERY_ME = gql`
   query me {
@@ -26,6 +16,8 @@ export const QUERY_ME = gql`
       _id
       username
       email
+      games
+      wins
     }
   }
 `;
