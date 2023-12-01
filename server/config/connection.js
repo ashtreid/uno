064b830dcd05fe0d09314cea62f05f5f89f89476
Ash Reid <ashley.reid013@gmail.com>
2023-11-30 19:35:02 -0700

    kill socket

--- server/config/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-// Database name: unoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/unoDB');
-
-module.exports = mongoose.connection;
--- server/models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require('./User');
-const Game = require('./Game');
-const Card = require('./Card');
-
-module.exports = { User, Game, Card };
