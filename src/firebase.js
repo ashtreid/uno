c7c0450f6bfe73a0f4fd889c778cd4c07e9e3d34
Park <parkerlamarbrook@gmail.com>
2023-11-30 20:38:01 -0700

    updated firebase.js file with necessary configurations for database and auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,9 +13,14 @@ const firebaseConfig = {
   storageBucket: "unogame-c81ce.appspot.com",
   messagingSenderId: "1071789266719",
   appId: "1:1071789266719:web:9196e9de9cda7e73cfc22a",
-  measurementId: "G-J8GG9KR34Q"
+  measurementId: "G-J8GG9KR34Q",
+  databaseURL: "https://unogame-c81ce-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+const auth = getAuth(app);
