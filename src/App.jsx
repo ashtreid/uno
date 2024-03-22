b4d60cead9523b6b958f90edab50bd0525041cdf
Park <parkerlamarbrook@gmail.com>
2024-03-21 20:26:31 -0600

    cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import Home from './pages/Home';
-import Lobby from './pages/Lobby'; // Assuming you have a Lobby component
+import Lobby from './pages/Lobby';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,7 +14,6 @@ function App() {
     <Router>
       <>
         <div>
-          {/* Define your routes within <Routes> */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/lobby" element={<Lobby />} />
